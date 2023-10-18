47cf754951daada019abdcd8022013a298c1db99
codyivy0 <codyivy0@gmail.com>
2023-10-18 13:25:07 -0500

    set up

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Main = (props) => {
+  return (
+   <main className="box">
+    <h1>This is the main</h1>
+    <ul>
+        <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta nesciunt commodi non labore. Nesciunt provident laudantium at doloribus unde explicabo dolorem in assumenda, eveniet voluptatem perferendis consequuntur ducimus voluptatum aut!
+        </li>
+        <li>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, quos sed iusto aspernatur quae obcaecati deserunt quidem libero exercitationem non, in, ipsum praesentium saepe. Sed dicta blanditiis earum doloribus reiciendis!
+        </li>
+    </ul>
+   </main>
+  )
+};
+
+export default Main;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = (props) => {
+  return (
+    <div className="side-bar box">
+      <h1>Notes v3.0</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
